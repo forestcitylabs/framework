f9d0e1980981bde493241bd0471158769d18a3dd
Luke Bainbridge <luke.bainbridge@gmail.com>
2023-11-30 13:16:21 -0500

    Adding filesystem driver for sessions.

---
+++ src/Session/Driver/FilesystemDriverMiddleware.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ForestCityLabs\Framework\Session\Driver;
+
+use ForestCityLabs\Framework\Session\Session;
+use ForestCityLabs\Framework\Session\SessionDriverInterface;
+use League\Flysystem\Filesystem;
+use League\Flysystem\FileAttributes;
+use Ramsey\Uuid\UuidInterface;
+
+class FilesystemDriverMiddleware implements SessionDriverInterface
+{
+    public function __construct(
+        private Filesystem $filesystem,
+    ) {
+    }
+
+    public function load(UuidInterface $uuid): ?Session
+    {
+        $filename = $this->determineFilename($uuid);
+        if (!$this->filesystem->fileExists($filename)) {
+            return null;
+        }
+        $session = unserialize($this->filesystem->read($filename));
+        return $session;
+    }
+
+    public function save(Session $session): void
+    {
+        $filename = $this->determineFilename($session->getId());
+        $this->filesystem->write($filename, serialize($session));
+    }
+
+    public function delete(Session $session): void
+    {
+        $filename = $this->determineFilename($session->getId());
+        $this->filesystem->delete($filename);
+    }
+
+    public function deleteAll(): void
+    {
+        foreach ($this->filesystem->listContents('/', true) as $file) {
+            if ($file instanceof FileAttributes) {
+                $this->filesystem->delete($file->path());
+            }
+        }
+    }
+
+    public function loadAll(): iterable
+    {
+        foreach ($this->filesystem->listContents('/', true) as $file) {
+            if ($file instanceof FileAttributes) {
+                yield unserialize($this->filesystem->read($file->path()));
+            }
+        }
+    }
+
+    private function determineFilename(UuidInterface $uuid): string
+    {
+        return str_replace('-', DIRECTORY_SEPARATOR, $uuid->toString());
+    }
+}
