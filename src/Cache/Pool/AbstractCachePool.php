7ee740cbbcbb770ee12446481ff51b637865ca39
Luke Bainbridge <luke.bainbridge@gmail.com>
2023-12-07 13:16:23 -0500

    Removing unused use statement.

--- src/Cache/Pool/AbstractCachePool.php
+++ src/Cache/Pool/AbstractCachePool.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace ForestCityLabs\Framework\Cache\Pool;
 
-use ForestCityLabs\Framework\Cache\CacheItem;
 use ForestCityLabs\Framework\Cache\Exception\InvalidArgumentException;
 use Psr\Cache\CacheItemInterface;
 use Psr\Cache\CacheItemPoolInterface;
