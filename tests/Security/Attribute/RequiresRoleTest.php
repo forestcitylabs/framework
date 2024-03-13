3cdf40664d1229c3b6302825494800975abde981
Luke Bainbridge <luke.bainbridge@gmail.com>
2024-03-13 07:57:24 -0600

    Additional requires role coverage.

--- tests/Security/Attribute/RequiresRoleTest.php
+++ tests/Security/Attribute/RequiresRoleTest.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace ForestCityLabs\Framework\Tests\Security\Attribute;
 
 use ForestCityLabs\Framework\Security\Attribute\RequiresRole;
+use ForestCityLabs\Framework\Security\Exception\ForbiddenException;
 use ForestCityLabs\Framework\Security\Exception\UnauthorizedException;
+use ForestCityLabs\Framework\Security\Model\AccessTokenInterface;
+use ForestCityLabs\Framework\Security\Model\UserInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Test;
@@ -40,6 +43,27 @@ class RequiresRoleTest extends TestCase
             ->with('_access_token')
             ->willReturn(new stdClass());
         $this->expectException(UnauthorizedException::class);
+        $attribute->checkRequirement($request);
+    }
+
+    #[Test]
+    public function userDoesNotHaveRole(): void
+    {
+        // Create the attribute.
+        $attribute = new RequiresRole('admin');
+
+        // Mock the values.
+        $request = $this->createMock(ServerRequestInterface::class);
+        $token = $this->createMock(AccessTokenInterface::class);
+        $user = $this->createMock(UserInterface::class);
+
+        // Configure the values.
+        $request->method('getAttribute')->with('_access_token')->willReturn($token);
+        $token->method('getUser')->willReturn($user);
+        $user->method('hasRole')->with('admin')->willReturn(false);
+
+        // Expect the forbidden exception.
+        $this->expectException(ForbiddenException::class);
         $attribute->checkRequirement($request);
     }
 }
