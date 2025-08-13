77f12de567a654a64940a3ea8691e135a0768eae
Luke Bainbridge <luke.bainbridge@gmail.com>
2025-08-13 16:25:34 -0400

    Additional claims resolution.

--- src/Security/Oidc/Grant/AuthorizationCodeGrant.php
+++ src/Security/Oidc/Grant/AuthorizationCodeGrant.php
@@ -6,13 +6,17 @@ namespace ForestCityLabs\Framework\Security\Oidc\Grant;
 
 use DateInterval;
 use DateTimeImmutable;
-use ForestCityLabs\Framework\Security\ClientManagerInterface;
 use ForestCityLabs\Framework\Security\Exception\OAuthException;
 use ForestCityLabs\Framework\Security\Manager\AccessTokenManagerInterface;
 use ForestCityLabs\Framework\Security\Manager\AuthCodeManagerInterface;
+use ForestCityLabs\Framework\Security\Manager\ClientManagerInterface;
 use ForestCityLabs\Framework\Security\Manager\RefreshTokenManagerInterface;
+use ForestCityLabs\Framework\Security\Model\ClientInterface;
 use ForestCityLabs\Framework\Security\OAuth\AuthRequest;
 use ForestCityLabs\Framework\Security\OAuth\Grant\AuthorizationCodeGrant as OAuthAuthorizationCodeGrant;
+use ForestCityLabs\Framework\Security\OAuth\OAuthScopeRegistry;
+use ForestCityLabs\Framework\Security\Oidc\ClaimResolver;
+use ForestCityLabs\Framework\Security\Oidc\OidcClaimRegistry;
 use ForestCityLabs\Framework\Security\Oidc\OidcTokenResponse;
 use ForestCityLabs\Framework\Utility\SecureStringService;
 use Lcobucci\JWT\Configuration;
@@ -20,26 +24,35 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class AuthorizationCodeGrant extends OAuthAuthorizationCodeGrant
 {
-    private Configuration $jwt;
+    protected Configuration $jwt;
+    protected OidcClaimRegistry $claim_registry;
+    protected ClaimResolver $claim_resolver;
 
     public function __construct(
         Configuration $jwt,
+        OidcClaimRegistry $claim_registry,
+        ClaimResolver $claim_resolver,
         AuthCodeManagerInterface $auth_code_manager,
         AccessTokenManagerInterface $access_token_manager,
         RefreshTokenManagerInterface $refresh_token_manager,
         ClientManagerInterface $client_manager,
         SecureStringService $secure_string_service,
-        DateInterval $code_ttl = new DateInterval('P5M'),
-        DateInterval $access_token_ttl = new DateInterval('P1H'),
+        OAuthScopeRegistry $scope_registry,
+        DateInterval $code_ttl = new DateInterval('PT5M'),
+        DateInterval $access_token_ttl = new DateInterval('PT1H'),
         DateInterval $refresh_token_ttl = new DateInterval('P1M'),
     ) {
         $this->jwt = $jwt;
+        $this->claim_registry = $claim_registry;
+        $this->claim_resolver = $claim_resolver;
+
         parent::__construct(
             $auth_code_manager,
             $access_token_manager,
             $refresh_token_manager,
             $client_manager,
             $secure_string_service,
+            $scope_registry,
             $code_ttl,
             $access_token_ttl,
             $refresh_token_ttl
@@ -73,14 +86,22 @@ class AuthorizationCodeGrant extends OAuthAuthorizationCodeGrant
         $scopes = explode(' ', $auth_request->getScope());
         if (in_array('openid', $scopes, true)) {
             $now = new DateTimeImmutable();
-            $id_token = $this->jwt->builder()
+            $builder = $this->jwt->builder()
                 ->issuedBy($request->getUri()->getScheme() . '://' . $request->getUri()->getHost())
                 ->permittedFor($auth_request->getClientId())
                 ->relatedTo($response->getAccessToken()->getUser()->getIdentifier())
                 ->issuedAt($now)
                 ->expiresAt($now->add($this->access_token_ttl))
-                ->withClaim('nonce', $auth_request->getNonce())
-                ->getToken($this->jwt->signer(), $this->jwt->signingKey());
+                ->withClaim('nonce', $auth_request->getNonce());
+            foreach ($this->claim_resolver->resolveClaims($scopes, $response->getAccessToken()->getUser()) as $claim => $value) {
+                // Add the claim to the ID token.
+                $builder = $builder->withClaim($claim, $value);
+            }
+
+            $id_token = $builder->getToken(
+                $this->jwt->signer(),
+                $this->jwt->signingKey()
+            );
         }
 
         // Return the response with the ID token added.
@@ -89,5 +110,22 @@ class AuthorizationCodeGrant extends OAuthAuthorizationCodeGrant
             $response->getRefreshToken(),
             $id_token ?? null
         );
+    }
+
+    public function validateScopes(array $scopes, ClientInterface $client): void
+    {
+        // Validate the requested scopes against the registered scopes.
+        foreach ($scopes as $scope) {
+            if (
+                !$this->scope_registry->isValidScope($scope)
+                && !$this->claim_registry->isValidClaim($scope)
+                && !$this->claim_registry->isValidGroup($scope)
+            ) {
+                throw new OAuthException("Invalid scope: $scope");
+            }
+            if (!in_array($scope, $client->getScopes(), true)) {
+                throw new OAuthException(sprintf('Invalid scope "%s" for client.', $scope));
+            }
+        }
     }
 }
