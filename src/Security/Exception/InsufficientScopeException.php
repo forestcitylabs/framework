666f5e3a9b251d4c619af3de19886cc3b0fcd31a
Luke Bainbridge <luke.bainbridge@gmail.com>
2025-10-07 09:39:19 -0400

    Better scope extension detail.

--- src/Security/Exception/InsufficientScopeException.php
+++ src/Security/Exception/InsufficientScopeException.php
@@ -4,4 +4,8 @@ namespace ForestCityLabs\Framework\Security\Exception;
 
 class InsufficientScopeException extends ForbiddenException
 {
+    public function getDetail(): string
+    {
+        return 'insufficient_scope';
+    }
 }
